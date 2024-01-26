46ae6bb1e2f8f14cde0962ab5e4adda4ef0e9d55
Pacharapon <sundaynight.1919@gmail.com>
2024-01-26 18:57:41 +0700

    create simple nn class

---
+++ example/index.ts
@@ -0,0 +1,33 @@
+import NeuralNetwork from "../src/nn";
+
+const net = new NeuralNetwork(2, 4, 1);
+
+const inputs = [
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1],
+]
+
+const outputs = [
+    [0],
+    [1],
+    [1],
+    [0],
+]
+
+const ephochs = 100000;
+
+for (let i = 0; i < ephochs; i++) {
+    for (let i = 0; i < inputs.length; i++) {
+        net.train(inputs[i], outputs[i]);
+    }
+
+    const output = net.predict([1, 0]);
+
+    console.log(output)
+}
+
+const output = net.predict([1, 0]);
+
+console.log(output)
